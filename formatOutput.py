d93c3fc626e8a538c3ebb886b1da62a3cb7cdab4
Ubuntu <ubuntu@ip-172-31-6-152.us-west-2.compute.internal>
2014-05-27 21:42:37 +0000

    formatting output

--- formatOutput.py
+++ formatOutput.py
@@ -1,6 +1,14 @@
+import re
+
+
 try:
-	with open('output_raw.txt') as outputFile:
-		for line in outputFile:
-			print line
+    f = open('output_formatted.txt', 'a')
+    with open('output_raw.txt') as outputFile:
+        for line in outputFile:
+            print line
+            match = re.search("real\t\dm(\d+.\d+)s", line)
+            if match:
+                f.write(match.group(1))
+    f.close()
 except EnvironmentError:
-        print 'Error reading file'
+    print 'Error reading file'
