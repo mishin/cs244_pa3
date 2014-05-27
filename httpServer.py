bf36ac762852c3ded3e593f91ad17ed3eb8d3e8a
Ubuntu <ubuntu@ip-172-31-2-195.us-west-2.compute.internal>
2014-05-27 18:58:00 +0000

    removed old server file

--- httpServer.py
+++ httpServer.py
@@ -1,26 +1,39 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import os
 import random
 
-#Create custom HTTPRequestHandler class
+""" Create custom HTTPRequestHandler class
+"""
 class KodeFunHTTPRequestHandler(BaseHTTPRequestHandler):
-    #handle GET command
-    def do_GET(self):
-        #send code 200 response
-        self.send_response(200)
-        #send header first
-        self.send_header('Content-type','text-html')
-        self.end_headers()
-        #generate random string
-        length = get_response_size()
-        payload = 'a'*length
 
-        #send file content to client
-        self.wfile.write(payload)
+    # handle GET command
+    def do_GET(self):
+        try:
+            # send code 200 response
+            self.send_response(200)
+            
+            # send header first
+            self.send_header('Content-type', 'text-html')
+            self.end_headers()
+            
+            # random string
+            resp_size = get_response_size()
+            response = 'a'*resp_size
+            
+            # send string to client
+            self.wfile.write(response)
+            return
+        
+        except IOError:
+            self.send_error(404, 'file not found')
 
-def get_response_size():    # returns in bytes
+""" return response size in bytes
+"""
+#TODO: need finer distribution
+def get_response_size():
     sample = random.uniform(0, 1)
+    
     if sample < 0.25:
         return 200
     elif sample < 0.5:
@@ -29,16 +42,16 @@ def get_response_size():    # returns in bytes
         return 5000
     else:
         return 50000    
-     
+    
 def run():
-    print('http server is starting...')
- 
-    #ip and port of servr
-    #by default http server port is 80
-    server_address = ('127.0.0.1', 80)
-    httpd = HTTPServer(server_address, KodeFunHTTPRequestHandler)
-    print('http server is running...')
-    httpd.serve_forever()
-     
+    print('http server is starting...')
+    
+    #ip and port of server
+    #by default http server port is 80
+    server_address = ('127.0.0.1', 80)
+    httpd = HTTPServer(server_address, KodeFunHTTPRequestHandler)
+    print('http server is running...')
+    httpd.serve_forever()
+    
 if __name__ == '__main__':
-    run()
+    run()
